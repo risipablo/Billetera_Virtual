c313ca7e9ec274c19ac2bb0aeaf048d555b723be
Pablo Risi <124108279+risipablo@users.noreply.github.com>
2024-08-09 18:45:35 -0300

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://billetera-virtual-eight.vercel.app'],
     optionsSuccessStatus: 200
 };
 
