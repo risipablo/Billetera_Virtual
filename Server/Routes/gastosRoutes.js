2e33c34f8ef1fcd224cb59a6c26abc26a2372735
Pablo Risi <124108279+risipablo@users.noreply.github.com>
2024-10-01 19:43:31 -0300

    Update gastosRoutes.js

--- Server/Routes/gastosRoutes.js
+++ Server/Routes/gastosRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getGastos, addGasto, deleteGasto, editGasto } = require('../controllers/gastosController');
+const { getGastos, addGasto, deleteGasto, editGasto } = require('../Controllers/gastosController');
 
 // Proteger rutas
 const {protect, isAdmin} = require ('../Middleware/authMiddleware')
