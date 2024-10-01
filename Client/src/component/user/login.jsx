4ce3d2a804ec31c42c73751248dff489ac415f1a
Pablo Risi <pablomrisi@hotmail.com>
2024-10-01 17:24:38 -0300

    render pruebas login

--- Client/src/component/user/login.jsx
+++ Client/src/component/user/login.jsx
@@ -5,8 +5,7 @@ import { useNavigate } from 'react-router-dom';
 import "./user.css"
 
 // const serverFront = "http://localhost:3001";
-const serverFront = process.env.REACT_APP_SERVER_URL || "http://localhost:3001";
-
+const serverFront =  'https://billetera-virtual.onrender.com';
 
 
 const Login = ({ setIsAuthenticated }) => {
