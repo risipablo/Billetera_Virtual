a4e24e6bdde99b07ba76cc8fa35ae55b0430bf81
Pablo Risi <pablomrisi@hotmail.com>
2024-10-02 18:08:36 -0300

    cors

--- Server/index.js
+++ Server/index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://billetera-virtual-nine.vercel.app', ],
+    origin: ['http://localhost:5173', 'https://billetera-virtual-nine.vercel.app','https://billetera-virtual-1.onrender.com' ],
     optionsSuccessStatus: 200,
     methods: 'GET,POST,DELETE,PATCH',
     credentials: true,
