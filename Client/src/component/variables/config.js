ea2f2f288e4658f2fbe90e8461e663aeaee28458
Pablo Risi <pablomrisi@hotmail.com>
2026-07-16 20:18:52 -0300

    variables railway

--- Client/src/component/variables/config.js
+++ Client/src/component/variables/config.js
@@ -1,6 +1,7 @@
 export const config = {
     // apiUrl: "http://localhost:3001", 
-    apiUrl: "https://billetera-virtual-1.onrender.com"
+    // apiUrl: "https://billetera-virtual-1.onrender.com"
+    apiUrl: "https:billeteravirtual-production.up.railway.app"
 };
 
 
