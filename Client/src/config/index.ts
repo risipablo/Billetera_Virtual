7cb6eddacaa8e40b03b96a8016e160053e450685
Pablo Risi <pablomrisi@hotmail.com>
2026-08-31 21:59:31 -0300

    local

--- Client/src/config/index.ts
+++ Client/src/config/index.ts
@@ -1,9 +1,5 @@
 // config/index.ts
 export const config = {
-    Api: import.meta.env.VITE_API_URL || 'http://localhost:3001'
-    
+    // Api: 'http://localhost:3001'  // LOCAL
+    Api: 'https://billetera-virtual-1.onrender.com' // PRODUCCIÓN
 };
-
-console.log('🔍 API URL:', import.meta.env.VITE_API_URL)
-console.log('🔍 DEV:', import.meta.env.DEV)
-console.log('🔍 Config Api:', config.Api)
